fd48c56279ef7db38a9813e475c78cd853776706
LENOVO <oumaimabk168@gmail.com>
2021-03-06 14:54:34 +0100

    Hey!

--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,7 +15,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private  static  int SPLASH_SCREEN =5000; //means 5seconds
+    private  static  int SPLASH_SCREEN =3000; //means 5seconds
     //variables
     Animation topAnim, bottomAnim;
     ImageView imgLogo, imgText;
@@ -39,8 +42,16 @@ public class MainActivity extends AppCompatActivity {
            public void run() {
                //we call the other activity
                Intent intent = new Intent(MainActivity.this,login.class);
-               startActivity(intent);
-               finish();
+             //we crate a pair for the animation
+               Pair[] pairs = new Pair[1];
+               //the view is our image logo and the string is the name of the animation in the xml
+               pairs[0]= new Pair<View, String>(imgLogo,"logo_image_animation");
+               if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                   ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                //options.toBundle carry our animation intent call the next screen
+               startActivity(intent,options.toBundle());
+
+               }
            }
        },SPLASH_SCREEN);
 
--- app/src/main/java/com/example/contactapp/login.java
+++ app/src/main/java/com/example/contactapp/login.java
@@ -2,13 +2,57 @@ package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class login extends AppCompatActivity implements View.OnClickListener {
+    //private TextInputEditText txtUsername, txtPasswd ;
+    private TextInputLayout inputUsername, inputPasswd;
+    Button btnGo, btnLeave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inputUsername = findViewById(R.id.txt_username);
+        inputPasswd = findViewById(R.id.txt_password);
+        btnGo = findViewById(R.id.btn_go);
+        btnLeave = findViewById(R.id.btn_leave);
+
+        /*txtUsername = findViewById(R.id.txt_username);
+        txtPasswd = findViewById(R.id.txt_password);
+        TextInputLayout textInputLayout = findViewById(R.id.custom_end_icon);
+         String text = textInputLayout.getEditText().getText();
+        btnGo = findViewById(R.id.btn_go);
+        btnLeave = findViewById(R.id.btn_leave);
+*/
+
+        btnGo.setOnClickListener(this);
+        btnLeave.setOnClickListener(this);
+    }
+
+   @Override
+    public void onClick(View v) {
+        if(v==btnLeave){
+            this.finish();
+        }else {
+            String nom = inputUsername.getEditText().getText().toString();
+            String pswd =  inputPasswd.getEditText().getText().toString();
+            if(nom.equalsIgnoreCase("oumaima")&&pswd.equals("0000")){
+                Intent inte = new Intent(login.this,HomePage.class);
+                inte.putExtra("Username", nom);
+                startActivity(inte);
+            }
+            else{
+                //lancer un msg d'errreur
+                Toast.makeText(login.this,"wrong username or password",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
